0c84f7a74d30e4616e6cebb391eeee204b7a746e
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2017-05-29 17:20:17 +0200

    getNeighbors bugfix

--- cifar10_natenc_getNeighbors.py
+++ cifar10_natenc_getNeighbors.py
@@ -5,22 +5,25 @@ np.random.seed(3333)
 import utils
 
 
-params = dict(test_batch_size=32,
+params = dict(batch_size=32,
               model_dir='/media/jan/DataExt4/pycharm/natenc-cifar10-allclass',
               data_dir='/media/jan/DataExt4/BenchmarkDataSets/Classification/cifar-10-batches-py',
-              out_path='/home/jan/Documents/GANResults/natenc_neighbors/test_arange.png',
+              out_path='/home/jan/Documents/GANResults/natenc_neighbors/neighbors.png',
               input_type='cifar10',
-              use_grayscale=True,
-              use_gradient_images=True,
-              z_dim=16,
+              lr=0.0,
+              mlp_lr=0.0,
+              use_grayscale=False,
+              use_gradient_images=False,
+              augment_mlp_training=False,
+              z_dim=32,
               num_classes=10)
 
 
 num_tests = 1
-samples = [0,10,20]
+samples = [10,20,30,60,70,90]
 number_of_neighbors = 5
 
-test_batch_size = params['test_batch_size']
+test_batch_size = params['batch_size']
 
 #load cifar10
 data_train, labels_train, data_test, labels_test = utils.load_cifar_XandY(params['data_dir'])
@@ -28,7 +31,7 @@ data_train, labels_train, data_test, labels_test = utils.load_cifar_XandY(params
 data_test_prep = data_test
 
 # setup gan
-net_enc = model.NATEnc(params)
+nat_enc = model.NATEnc(params)
 # tf.global_variables_initializer().run()
 
 summary_writer = tf.summary.FileWriter(params['model_dir'])
@@ -37,15 +40,16 @@ sv = tf.train.Supervisor(logdir=params['model_dir'], summary_writer=None)
 gpu_options = tf.GPUOptions(allow_growth=True)
 sess_config = tf.ConfigProto(allow_soft_placement=True,
                             gpu_options=gpu_options)
-lr = params['lr']
+
 # set up the TF session and init all ops and variables
 with sv.prepare_or_wait_for_session(config=sess_config) as sess:
     cycles = data_test_prep.shape[0]/test_batch_size
     results = []
     for i in range(cycles):
-        feed_dict = {net_enc.input_test_ph: data_test_prep[i*test_batch_size:(i+1)*test_batch_size]}
+        feed_dict = {nat_enc.input_test_ph: data_test_prep[i*test_batch_size:(i+1)*test_batch_size],
+                     nat_enc.dropout_keep_prob:1.0}
         fetch_dict = {
-                "reps": net_enc.representation_test
+                "reps": nat_enc.representation_test,
             }
         res_test = sess.run(fetch_dict,feed_dict=feed_dict)
         results.append(res_test['reps'])
